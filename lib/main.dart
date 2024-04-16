4c1c6ebfcbe9d85bd40f98866ddf49d93de3a979
lhyeonnit <lhyeonnit@gmail.com>
2024-04-16 17:31:30 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,14 @@ import 'package:airbnb_app/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomePage(),
     );
   }
