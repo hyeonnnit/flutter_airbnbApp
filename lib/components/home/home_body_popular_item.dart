9640d28e9ab51147a6e077dcdead825156a387c0
lhyeonnit <lhyeonnit@gmail.com>
2024-04-16 16:46:34 +0900

    HomeBodyPopularItem

---
+++ lib/components/home/home_body_popular_item.dart
@@ -0,0 +1,48 @@
+import 'package:airbnb_app/size.dart';
+import 'package:flutter/material.dart';
+
+class HomeBodyPopularItem extends StatelessWidget {
+  final id;
+  final popularList = [
+    "p1.jpeg",
+    "p2.jpeg",
+    "p3.jpeg",
+  ];
+  HomeBodyPopularItem({required this.id});
+
+  @override
+  Widget build(BuildContext context) {
+    double popularItemWidth = getBodyWidth(context)/ 3 - 5;
+    return Padding(
+      padding: const EdgeInsets.only(bottom: gap_xl),
+      child: Container(
+        constraints: BoxConstraints(
+          minWidth: 320,
+        ),
+        child: SizedBox(
+          width: popularItemWidth,
+          child: Column(
+            children: [
+              _buildPopularItemImage(),
+              _buildPopularItemStar(),
+              _buildPopularItemComment(),
+              _buildPopularItemUserInfo(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _buildPopularItemImage(){
+    return SizedBox();
+  }
+  Widget _buildPopularItemStar(){
+    return SizedBox();
+  }
+  Widget _buildPopularItemComment(){
+    return SizedBox();
+  }
+  Widget _buildPopularItemUserInfo(){
+    return SizedBox();
+  }
+}
