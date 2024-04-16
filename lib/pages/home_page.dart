05ad60c6f1dbcf30464edc73a27766038dd73727
lhyeonnit <lhyeonnit@gmail.com>
2024-04-16 17:20:22 +0900

    HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,17 @@
+import 'package:airbnb_app/components/home/home_body.dart';
+import 'package:airbnb_app/components/home/home_header.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({
-    super.key,
-  });
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: ListView(
+        children: [
+          HomeHeader(),
+          HomeBody(),
+        ],
+      ),
+    );
   }
 }
