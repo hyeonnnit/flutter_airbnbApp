f1ba4f763a9f5c2ba898c1bbf7d04d8f05e55c54
lhyeonnit <lhyeonnit@gmail.com>
2024-04-17 12:35:11 +0900

    HomeHeaderForm

--- lib/components/home/home_header_form.dart
+++ lib/components/home/home_header_form.dart
@@ -8,10 +8,11 @@ import 'package:flutter/material.dart';
 class HomeHeaderForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.only(top: gap_m),
       child: Align(
-        alignment: Alignment(-0.6, 0),
+        alignment: screenWidth < 520 ?  Alignment(0, 0): Alignment(-0.6, 0),
         child: Container(
           width: 420,
           decoration: BoxDecoration(
