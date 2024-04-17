05f9c5ce7cde4e4c612eccfa4d338aa8a1b9d8da
lhyeonnit <lhyeonnit@gmail.com>
2024-04-17 12:31:26 +0900

    _buildPopularTitle

--- lib/components/home/home_body_popular.dart
+++ lib/components/home/home_body_popular.dart
@@ -1,5 +1,6 @@
 import 'package:airbnb_app/components/home/home_body_popular_item.dart';
 import 'package:airbnb_app/size.dart';
+import 'package:airbnb_app/styles.dart';
 import 'package:flutter/cupertino.dart';
 
 class HomeBodyPopular extends StatelessWidget {
@@ -19,7 +20,19 @@ class HomeBodyPopular extends StatelessWidget {
   }
 
   Widget _buildPopularTitle() {
-    return SizedBox();
+    return Column(
+      children: [
+        Text(
+          "한국 숙소에 직접 다녀간 게스트의 후기",
+          style: h5(),
+        ),
+        Text(
+          "게스트 후기 2,500,000개 이상, 평균 평점 4.7점(5점 만점)",
+          style: body1(),
+        ),
+        SizedBox(height: gap_m),
+      ],
+    );
   }
 
   Widget _buildPopularList() {
