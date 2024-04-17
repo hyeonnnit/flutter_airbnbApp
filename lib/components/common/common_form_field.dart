958709f777dfe7aa6872383b47cec27f9cd41f55
lhyeonnit <lhyeonnit@gmail.com>
2024-04-17 09:35:36 +0900

    CommonFormField

--- lib/components/common/common_form_field.dart
+++ lib/components/common/common_form_field.dart
@@ -1,9 +1,43 @@
+import 'package:airbnb_app/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CommonFormField extends StatelessWidget {
+  final prifixText;
+  final hintText;
+
+  const CommonFormField({required this.hintText, required this.prifixText});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        TextFormField(
+          textAlignVertical: TextAlignVertical.bottom,
+          decoration: InputDecoration(
+            contentPadding: EdgeInsets.only(top: 30, left: 20, bottom: 10),
+            hintText: hintText,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: BorderSide(
+                color: Colors.black,
+                width: 2,
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 8,
+          left: 20,
+          child: Text(
+            prifixText,
+            style: overLine(),
+          ),
+        ),
+      ],
+    );
   }
 }
