4caeddf80b7096f1625da469a9cc0fa7b6be1653
lhyeonnit <lhyeonnit@gmail.com>
2024-04-17 09:21:49 +0900

    _buildAppBarLogo, _buildAppBarMenu

--- lib/components/home/home_header_appbar.dart
+++ lib/components/home/home_header_appbar.dart
@@ -1,5 +1,8 @@
+import 'package:airbnb_app/constants.dart';
 import 'package:airbnb_app/size.dart';
+import 'package:airbnb_app/styles.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeHeaderAppBar extends StatelessWidget {
 
@@ -18,10 +21,36 @@ class HomeHeaderAppBar extends StatelessWidget {
   }
 
   Widget _buildAppBarLogo(){
-    return SizedBox();
+    return Row(
+      children: [
+        Image.asset(
+          "assets/logo.png",
+          width: 30,
+          height: 30,
+          color: kAccentColor,
+        ),
+        SizedBox(width: gap_s),
+        Text(
+            "RoomOfAll",
+          style: h5(mColor: Colors.white),
+        ),
+      ],
+    );
   }
 
   Widget _buildAppBarMenu(){
-    return SizedBox();
+    return Row(
+      children: [
+        Text(
+          "회원가입",
+          style: subtitle1(mColor: Colors.white),
+        ),
+        SizedBox(width: gap_m),
+        Text(
+            "로그인",
+          style: subtitle1(mColor: Colors.white),
+        ),
+      ],
+    );
   }
 }
